88703b9cccc7eabee2cf9f6a56923dcc19f90fc2
Colin Alford <colinalford@gmail.com>
2015-05-29 14:36:26 -0500

    Add methods for controller and model. Extensive work done. Should have committed more and sooner. Oh well. Removed testing framework. Need to learn more about it before I do that. Will use TDD for next project.

--- js/script.js
+++ js/script.js
@@ -2,93 +2,132 @@
 
 var model = {
 
-	currentDate : function () {
-		var today = new Date();
-		var month = today.getMonth()+1;
-		return today.getFullYear().toString() + "-" + month.toString() + "-" + today.getDate().toString();
-	},
-
 	dates : {},
 
-	addDate: function (date) {
-		this.dates[date] = {};
-		this.dates[date].tasks = [];
-		this.dates[date].events = [];
-		this.dates[date].notes = [];
+	init: function () {
+		model.addDate(controller.today());
 	},
 
-	addTask : function(date, task) {
-		this.dates[date].tasks.push(task);
+	addDate: function (dateObj) {
+		var formatted = controller.formatDate(dateObj);
+		if(this.dates[formatted] === undefined) {
+			this.dates[formatted] = {};
+		}
+		this.dates[formatted].tasks = [];
+		this.dates[formatted].events = [];
+		this.dates[formatted].notes = [];
 	},
 
-	addEvent : function(date, event) {
-		this.dates[date].events.push(event);
+	addTask : function(dateObj, task) {
+		var formatted = controller.formatDate(dateObj);
+		model.addDate(formatted);
+		if (this.dates[formatted].tasks === undefined) {
+			this.dates[formatted].tasks = [];
+		}
+		this.dates[formatted].tasks.push([controller.timestamp(), task]);
 	},
 
-	addNote : function(date, note) {
-		this.dates[date].notes.push(note);
+	addEvent : function(dateObj, event) {
+		var formatted = controller.formatDate(dateObj);
+		model.addDate(formatted);
+		if (this.dates[formatted].events === undefined) {
+			this.dates[formatted].events = [];
+		}
+		this.dates[formatted].events.push([controller.timestamp(), event]);
 	},
+
+	addNote : function(dateObj, note) {
+		var formatted = controller.formatDate(dateObj);
+		model.addDate(formatted);
+		if (this.dates[formatted].notes === undefined) {
+			this.dates[formatted].notes = [];
+		}
+		this.dates[formatted].tasks.push([controller.timestamp(), note]);
+	}
 };
 
 var view = {
-	displayMonth : function () {
-		view.displayEvents(date, event);
-		view.displayTasks(date, task);
+
+	init : function () {
+		
 	},
-	displayDay : function () {
-		view.displayTasks(date, task);
-		view.displayEvents(date, event);
-		view.displayNotes(date, note);
-	},
+
+	displayMonth : function () {},
+
+	displayDay : function () {},
+
 	displayTasks : function () {},
 	displayEvents : function () {},
 	displayNotes : function () {},
 };
 
 var controller = {
-	getDate : function (){},
-	getTask : function (){},
-	getNote : function (){},
-	getEvent : function (){}
-}
 
+	init : function() {
+		model.init();
+	},
 
-var i = 10;
+	formatDate : function (dateObj) {
+		var day = dateObj.getDate().toString();
+		var month = (dateObj.getMonth()+1).toString();
+		var year = dateObj.getFullYear().toString();
+		return year + "-" + month + "-" + day;
+	},
 
-while (i > 0) {
-	var datum = "2015-12-"+i;
-	var t_task = "Lorem ipsum dolor sit amet.";
-	var t_event = "Lorem ipsum dolor sit amet, consectetur adipisicing.";
-	var t_note = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente aliquid quaerat amet hic, mollitia iste!";
-	model.addDate(datum);
-	model.addTask(datum, t_task);
-	model.addEvent(datum, t_event);
-	model.addNote(datum, t_note);
-	i = i - 1;
-}
+	today : function () {
+		var today = new Date();
+		return today;
+	},
 
-var myDate = new Date();
-model.addDate(model.currentDate);
-model.addTask(model.currentDate, "Get adfasdffasdfa");
-model.addTask(model.currentDate, "asdfasdfadsfadsfads");
-model.addTask(model.currentDate, "Swgasdf;lkghas");
-alert(model.dates[model.currentDate].tasks);
-/*
+	timestamp : function(){
+		return Date.now();
+	},
 
+	month : function(dateObj) {
+		var monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+		return monthNames[dateObj.getMonth()];
+	},
 
-//View
-function View () {}
+	numberOfDays : function(dateObj) {
+		var thirtyOne = [0,2,4,6,7,9,11];
+		var thirty = [3,5,8,10];
 
-//Controller
-function Controller() {}
-
-var model = {
-	"date" : {
-		"2014-12-31" : {
-			"events" : [],
-			"tasks" : [],
-			"notes" : []
+		if (thirtyOne.indexOf(dateObj.getMonth()) !== -1) {
+			return 31;
+		} else if (thirty.indexOf(dateObj.getMonth()) !== -1) {
+			return 30;
+		} else if (controller.isLeapYear(dateObj)) {
+			return 29;
+		} else {
+			return 28;
 		}
+	},
+
+	isLeapYear : function(dateObj) {
+		var year = dateObj.getFullYear();
+		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
+			return true;
+		} else {
+			return false;
+		}
+	},
+
+	getTasks : function (date) {
+		return model.dates[date].tasks;
+	},
+
+	getEvents : function(date) {
+		return model.dates[date].events;
+	},
+
+	getNotes : function(date) {
+		return model.dates[date].events;
 	}
-}
-*/
+};
+
+
+model.init();
+
+
+
+
